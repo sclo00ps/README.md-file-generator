e1b0f6a714f7a0b4fcac5fe8a309bb6cd5428688
sclo00ps <louginescott@gmail.com>
2021-06-10 00:34:44 -0400

    js file

--- index.js
+++ index.js
@@ -1,11 +1,86 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+inquirer 
+.prompt ([
+    {
+        type: 'input',
+        message: 'What is your project title?',
+        name:  'title',
+    },
 
+    {
+        type: 'input',
+        message: 'What is the project description?',
+        name:  'description',
+    },
+    {    
+        type: 'input',
+        message: 'What is the usage information?',
+        name:  'usage',
+    },  
+    {    
+        type: 'input',
+        message: 'What are the conribution guidelines?',
+        name:  'contributions',
+    },
+    {
+        type: 'input',
+        message: 'What are the test instructions?',
+        name:  'tests',
+    },
+    {
+        type: 'list',
+        message: 'What is your license type?',
+        name:  'license',
+        choices: ["MIT", "Apache", "GPL"]
+    },
+
+    {
+        type: 'input',
+        message: 'What is your Github username?',
+        name:  'username',
+    },
+    {
+        type: 'input',
+        message: 'What is your email address?',
+        name:  'email',
+    },
+
+    ])
+    .then((answers) => {
+       // const filename = '${answers.name.toLowerCase().split(' ').join('')}.json';
+      //  const  - revisit this on pg 74 of notes
+        answers.input === answers.title,
+        answers.input === answers.description,
+        answers.input === answers.usage,
+        answers.input === answers.contributions,
+        answers.input === answers.tests,
+        answers.input === answers.license,
+        answers.input === answers.username,
+        answers.input === answers.email
+          //  console.log(answers);       
+
+    // TODO: Create a function to write README file - figure out 
+    //to write to questionns section for some of the entries githb un,
+    //email to ques, with instructions on additional ques. text
+    // when click on links in TOC, taken to sections of RM file - create
+    //string file for writeFile
+fs.writeFile("log.txt", r,  (err) =>
+    err ? console.log(err) : console.log ("README file created successfully!") 
+
+)    
+})
+
+
+
+
+   
+       
 // TODO: Create a function to initialize app
 function init() {}
 
